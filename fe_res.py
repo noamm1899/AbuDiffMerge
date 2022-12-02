ab54184f782d99849fc278be1ad9830e9a022ec3
noamm1899 <noam.kochavi@gmail.com>
2022-12-02 23:03:53 +0200

    added some color

--- fe_res.py
+++ fe_res.py
@@ -15,6 +15,20 @@ def pop(res):
         for j, val in enumerate(row):
             frame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
             label = tk.Label(master=frame, text=str(val))
+
+            if i == 0:
+                frame.config(bg="gray")
+                label.config(font="default 9 bold")
+            elif j == 2:
+                bg_col = ["red", "yellow", "green"][int(val != "ERROR") + int(val == "TRUE")]
+                fg_col = ["white", "black", "white"][int(val != "ERROR") + int(val == "TRUE")]
+                label.config(fg=fg_col, bg=bg_col)
+                frame.config(bg=bg_col)
+            elif j == 3:
+                bg_col = ["red", "green"][float(val) < 5]
+                fg_col = "white"
+                label.config(fg=fg_col, bg=bg_col)
+                frame.config(bg=bg_col)
             frame.grid(row=i, column=j, sticky="news")
             label.pack()
 
